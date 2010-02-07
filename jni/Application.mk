12bf191efac48e706c79fc2406170713cc49cbdf
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-02-07 14:31:40 +0100

    Workaround to make native lib compile with newer SDK version.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,5 @@
 APP_PROJECT_PATH := $(call my-dir)/..
 APP_MODULES      := libNativeTask
+
+# http://groups.google.com/group/android-ndk/browse_thread/thread/d20654a575001421/9c56ab28679751f0
+APP_PLATFORM := android-4

ba0923f11d4ed21f2050286d36b166a6ffdbd5f2
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-02-22 05:23:57 +0100

    Yet another attempt to fix the su-hanging issue: We know pipe commands into a su shell, like most other root apps seem to do. Didn't help though, even though we can run most commands fine. The issue indeed seems to be trying to 'pm', which differs in that it's a Java/Dalvik program run via app_process.

--- src/com/elsdoerfer/android/autostarts/ToggleTask.java
+++ src/com/elsdoerfer/android/autostarts/ToggleTask.java
@@ -1,9 +1,5 @@
 package com.elsdoerfer.android.autostarts;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
@@ -82,91 +78,43 @@ class ToggleTask extends ActivityAsyncTask<ListActivity, Object, Object, Boolean
 		// the user decided what to do).
 		mDoEnable = (Boolean)params[1];
 
-		// All right. So we can't use the PackageManager
-		// to disable components, since the permission
-		// required to do so has a protectLevel of
-		// "signature", meaning essentially that we need
-		// to be signed with the system certificate to
-		// be able to get it.
+		// All right. So we can't use the PackageManager to disable
+		// components, since the permission required to do so has a
+		// "protectLevel" of "signature", meaning essentially that we
+		// need to be signed with the system certificate to be granted
+		// it.
 		//
-		// Fortunately, there is a sort-of workaround.
-		// We can use the "pm" executable to communicate
-		// with the package manager, tell it to enable
-		// or disable a component. We need to do so as
-		// root, so that the PackageManager will skip the
-		// permission check (otherwise, it'll still look
-		// at the UID of the calling process), but it
-		// would work for root users.
+		// Fortunately, there is a sort-of workaround. We can use the
+		// "pm" executable to communicate with the package manager, tell
+		// it to enable or disable a component. We need to do so as root,
+		// so that the PackageManager will skip the permission check
+		// (otherwise, it'll still look at the UID of the calling
+		// process), but it would work for root users.
 		//
-		// However, there is another complication. Rooted
-		// devices these days often use custom firmware
-		// mods, and those often come with a special "su"
-		// executable, that asks the user for permission
-		// whenever a program wants to use su. This
-		// special su does support "Always allow", but
-		// considers the command arguments as well. In
-		// other words, since we need to use different
-		// arguments to "pm" every time, the user would
-		// be asked to confirm us being allowed to use
-		// "su" everytime.
-		//
-		// The workaround here is to write our command to
-		// a shell file, then in turn ask su to run that
-		// file.
-		final String scriptFile = "pm-call.sh";
-		FileOutputStream f;
-		try {
-			f = activity.openFileOutput(
-					scriptFile, ListActivity.MODE_PRIVATE);
-			try {
-				f.write(String.format("pm %s %s/%s",
-						(mDoEnable ? "enable": "disable"),
-						mApp.packageName, mApp.componentName).getBytes());
-				f.close();
 
-				// TODO: Temporary migration code, remove again.
-				// In the future, we could use this utility function to
-				// test for root; to make sure it won't look users out,
-				// during a test period let's log the result and we can
-				// have a look at the logs we get from people.
-				Log.i(ListActivity.TAG, "Testing for root: "+Utils.deviceHasRoot());
+		// TODO: Temporary migration code, remove again.
+		// In the future, we could use this utility function to
+		// test for root; to make sure it won't look users out,
+		// during a test period let's log the result and we can
+		// have a look at the logs we get from people.
+		Log.i(ListActivity.TAG, "Testing for root: "+Utils.deviceHasRoot());
 
-				Runtime r = Runtime.getRuntime();
-				Log.i(ListActivity.TAG, "Asking package manger to "+
-						"change component state to "+
-						(mDoEnable ? "enabled": "disabled"));
-				Process p = r.exec(new String[] {
-					"su", "-c", "sh "+activity.getFileStreamPath(scriptFile).getAbsolutePath() });
-				p.waitFor();
-				Log.d(ListActivity.TAG, "Process returned with "+p.exitValue());
-				Log.d(ListActivity.TAG, "Process stdout was: "+
-						Utils.readStream(p.getInputStream())+
-						"; stderr: "+
-						Utils.readStream(p.getErrorStream()));
+		Log.i(ListActivity.TAG, "Asking package manger to "+
+				"change component state to "+
+				(mDoEnable ? "enabled": "disabled"));
 
-				// In order to consider this a success, we require to
-				// things: a) a proper exit value, and ...
-				if (p.exitValue() != 0)
-					return false;
-
-				final PackageManager pm = activity.getPackageManager();
-				ComponentName c = new ComponentName(
-						mApp.packageName, mApp.componentName);
-				mApp.currentEnabled = pm.getComponentEnabledSetting(c);
-				return (mApp.isCurrentlyEnabled() == mDoEnable);
-			}
-			finally {
-				activity.deleteFile(scriptFile);
-			}
-		} catch (FileNotFoundException e) {
-			Log.e(ListActivity.TAG, "Failed to change state", e);
+		// Run the command; we are only happy if both the command itself
+		// succeed (proper return code) ...
+		if (!Utils.runRootCommand(String.format("pm %s %s/%s",
+			 	(mDoEnable ? "enable": "disable"),
+				mApp.packageName, mApp.componentName)))
 			return false;
-		} catch (IOException e) {
-			Log.e(ListActivity.TAG, "Failed to change state", e);
-			return false;
-		} catch (InterruptedException e) {
-			Log.e(ListActivity.TAG, "Failed to change state", e);
-			return false;
-		}
+
+		// ...and the state should now actually is what we expect.
+		final PackageManager pm = activity.getPackageManager();
+		ComponentName c = new ComponentName(
+				mApp.packageName, mApp.componentName);
+		mApp.currentEnabled = pm.getComponentEnabledSetting(c);
+		return (mApp.isCurrentlyEnabled() == mDoEnable);
 	}
 }
--- src/com/elsdoerfer/android/autostarts/Utils.java
+++ src/com/elsdoerfer/android/autostarts/Utils.java
@@ -1,6 +1,8 @@
 package com.elsdoerfer.android.autostarts;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,6 +10,8 @@ import java.io.Reader;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Set;
+
+import android.util.Log;
 
 public class Utils {
 	/**
@@ -60,5 +64,76 @@ public class Utils {
 	static boolean deviceHasRoot() {
     	File su = new File("/system/bin/su");
     	return su.exists();
+	}
+
+	/*
+	 * Running an app through root isn't even that straightforward as
+	 * it would seem. Here's some issues we've run into so far, and which
+	 * we try to workaround here:
+	 *
+	 *  1) The Superuser Whitelist application most rooted devices
+	 *     use identifies the command based on arguments. If we
+	 *     were to just call su -c "pm xyz", the user would need
+	 *     to confirm every single call; "Allows allow" would be
+	 *     useless.
+	 *
+	 *  2) su (or the Superuser Whitelist app) seem to have a bug
+	 *     that can cause them to freeze if "USB Debugging" is not
+	 *     enabled. It doesn't happen in every scenario, but when it
+	 *     does, it happens always.
+	 *
+	 *  3) Some custom ROMs contain what seems to be a kernel bug,
+	 *     in which su/sh?, when run outside of the system shell,
+	 *     cannot access certain paths. The error is "pm: not found".
+	 *     This happens even though the file does exists, and runs
+	 *     just fine from the shell.
+	 *
+	 * The common approach chosen by most root apps seems to be to
+	 * run "su" and pipe commands into it. This will solve at least
+	 * (1). It does't seem to affect (2). We'll have to see about (3).
+	 */
+	static boolean runRootCommand(String command) {
+		Process process = null;
+		DataOutputStream os = null;
+		try {
+			Log.d(ListActivity.TAG, "Running '"+command+"' as root");
+
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command+"\n");
+			os.writeBytes("echo \"rc\" $?\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+
+			Log.d(ListActivity.TAG, "Process returned with "+process.exitValue());
+			Log.d(ListActivity.TAG, "Process stdout was: "+
+				Utils.readStream(process.getInputStream())+
+				"; stderr: "+Utils.readStream(process.getErrorStream()));
+
+			// In order to consider this a success, we require to
+			// things: a) a proper exit value, and ...
+			if (process.exitValue() != 0)
+				return false;
+
+			return true;
+
+		} catch (FileNotFoundException e) {
+			Log.e(ListActivity.TAG, "Failed to change state", e);
+			return false;
+		} catch (IOException e) {
+			Log.e(ListActivity.TAG, "Failed to change state", e);
+			return false;
+		} catch (InterruptedException e) {
+			Log.e(ListActivity.TAG, "Failed to change state", e);
+			return false;
+		}
+		finally {
+			if (os != null)
+				try { os.close(); }
+				catch (IOException e) { throw new RuntimeException(e); }
+			if (process != null)
+				process.destroy();
+		}
 	}
 }

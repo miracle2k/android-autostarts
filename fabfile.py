ffc3965fb55fd7dea2d7a9325d4998d7535f9795
Michael Elsdörfer <michael@elsdoerfer.com>
2011-12-09 18:31:36 -0800

    Considerably reworked and improved build script.

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,16 @@ import time
 from os import path, unlink
 import logging
 
-from fabric.api import local, env
+from fabric.api import local, env, put, run
 from fabric.main import load_settings
 from android.build import AndroidProject, get_platform, ProgramFailedError
 
 
-env.raw_apk = 'bin/Android-Autostarts-%s.apk'
 settings = load_settings('.fabricrc')
 if not settings:
-     raise RuntimeError('.fabricrc is needed')
+    raise RuntimeError('.fabricrc is needed')
 env.update(settings)
+env.hosts = [env.deploy_host]
 
 
 logging.getLogger('py-androidbuild').addHandler(logging.StreamHandler())
@@ -27,14 +27,18 @@ def locales():
 BUILDS = {
     'amazon': {
         'extra-paths': ['src-opt/amazon'],
+        'template': 'com.elsdoerfer.android.autostarts-%(version)s-AD.apk',
+        'sign': False,
+        'deploy': False,
     },
     'default': {
         'extra-paths': ['src-opt/amazon'],
+        'template': 'Android-Autostarts-Full-%(version)s.apk',
     }
 }
 
 
-def build():
+def build(clean=False):
      apks = []
      for build_name, build_options in BUILDS.items():
          print "Building APK for version '%s'..." % build_name
@@ -44,37 +48,36 @@ def build():
              p.extra_source_dirs = build_options['extra-paths']
              p.extra_jars = ['%s/extras/android/support/v4/android-support-v4.jar' % env.sdk_dir]
 
-             apk = p.build(env.raw_apk % build_name.capitalize())
+             if clean:
+                 p.clean()
 
-             password = raw_input('Enter keystore password:')
-             if not password:
-                  print "Not signing the package."
-             else:
-                  apk.sign(env.keystore, env.keyalias, password,)
+             version = p.manifest_parsed.attrib['{http://schemas.android.com/apk/res/android}versionName']
+             apk = p.build('bin/%s' % build_options['template'] % {
+                 'build': build_name.capitalize(),
+                 'version': version
+             })
+
+             if build_options.get('sign', True):
+                 password = raw_input('Enter keystore password:')
+                 if not password:
+                      print "Not signing the package."
+                 else:
+                      apk.sign(env.keystore, env.keyalias, password)
              apk.align()
-             apks.append(_versionize(apk.filename))
+             apks.append((apk, build_options))
          except ProgramFailedError, e:
              print e
              print e.stdout
      return apks
 
 
-def _versionize(filename):
-    if not getattr(env, 'app_version', False):        
-        temp_project = AndroidProject('AndroidManifest.xml', sdk_dir=env.sdk_dir)
-        env.app_version = temp_project.manifest_parsed.attrib['{http://schemas.android.com/apk/res/android}versionName']
-
-    # add version number to file name
-    base, ext = path.splitext(filename)
-    target_apk = '%s-%s%s' % (base, env.app_version, ext)
-
-    # move to final location
-    local('mv %s %s' % (filename, target_apk))
-    return target_apk
-
-
 def deploy():
     locales()
-    for apk_filename in build():
-        local('scp -P 2211 %s %s' % (apk_filename, env.deploy_target))
+
+    for apk, build_options in build(clean=True):
+        if build_options.get('deploy', True):
+            put(apk.filename, env.deploy_dir)
+
+    put('CHANGES', env.deploy_dir)
+    run('sudo /etc/init.d/lighttpd restart')
 

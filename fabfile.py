8b8e1e5e4aa88b3d58495d31e99cb4addf66a18f
Michael Elsdörfer <michael@elsdoerfer.com>
2012-07-30 13:16:47 +0200

    fabfile: PEP8 fixes, some comments.

--- fabfile.py
+++ fabfile.py
@@ -18,26 +18,27 @@ logging.getLogger('py-androidbuild').addHandler(logging.StreamHandler())
 
 
 def locales():
-     local('tx pull -a')
-     # Workaround for Android not supporting this language (no ISO-639-2 support?)
-     local('rm locale/*-ast.po')
-     local('a2po import')
+    local('tx pull -a')
+    # Workaround for Android not supporting this language (no ISO-639-2 support?)
+    local('rm locale/*-ast.po')
+    local('a2po import')
 
 
 def _make_market_manifest(manifest):
-    """Stupid Android Market fails with mysterious error message 
+    """Stupid Android Market fails with mysterious error message
     ('cannot process apk' or something of the like) if the manifest
-    contains empty uses-configuration and uses-feature tags. 
+    contains empty uses-configuration and uses-feature tags.
     Amazon market fails if they do NOT exist.
     """
     manifest_text = open(manifest, 'r').read()
     manifest_text = re.sub(r'<uses-configuration\s*/>', '', manifest_text)
     manifest_text = re.sub(r'<uses-feature\s*/>', '', manifest_text)
-    # aapt will fail if the Manifest is not named AndroidManifest.xml    
+    # aapt will fail if the Manifest is not named AndroidManifest.xml
     new_manifest = path.join(tempfile.mkdtemp(), 'AndroidManifest.xml')
     with open(new_manifest, 'w') as f:
         f.write(manifest_text)
     return new_manifest
+
 
 BUILDS = {
     'amazon': {
@@ -55,41 +56,48 @@ BUILDS = {
 
 
 def build(clean=False):
-     apks = []
-     for build_name, build_options in BUILDS.items():
-         print "Building APK for version '%s'..." % build_name
-         try:
-             manifest = 'AndroidManifest.xml'
-             if build_options.get('manifest_maker', False):
-                 manifest = build_options['manifest_maker'](manifest)
-   
-             p = AndroidProject(manifest, 'Android-Autostarts',
-                                project_dir='.',
-                                sdk_dir=env.sdk_dir)
-             p.extra_source_dirs = build_options['extra-paths']
-             p.extra_jars = ['%s/extras/android/support/v4/android-support-v4.jar' % env.sdk_dir]
+    apks = []
+    for build_name, build_options in BUILDS.items():
+        print "Building APK for version '%s'..." % build_name
+        try:
+            # Determine manifest file, build can run a hook
+            manifest = 'AndroidManifest.xml'
+            if build_options.get('manifest_maker', False):
+                manifest = build_options['manifest_maker'](manifest)
 
-             if clean:
-                 p.clean()
+            # Setup a build project
+            p = AndroidProject(manifest, 'Android-Autostarts',
+                               project_dir='.',
+                               sdk_dir=env.sdk_dir)
+            p.extra_source_dirs = build_options['extra-paths']
+            p.extra_jars = ['%s/extras/android/support/v4/android-support-v4.jar' % env.sdk_dir]
 
-             version = p.manifest_parsed.attrib['{http://schemas.android.com/apk/res/android}versionName']
-             apk = p.build('%s' % build_options['template'] % {
-                 'build': build_name.capitalize(),
-                 'version': version
-             })
+            # Build the project
+            if clean:
+                p.clean()
+            version = p.manifest_parsed.attrib[
+            '{http://schemas.android.com/apk/res/android}versionName']
+            apk = p.build('%s' % build_options['template'] % {
+                'build': build_name.capitalize(),
+                'version': version
+            })
 
-             if build_options.get('sign', True):
-                 password = raw_input('Enter keystore password:')
-                 if not password:
-                      print "Not signing the package."
-                 else:
-                      apk.sign(env.keystore, env.keyalias, password)
-             apk.align()
-             apks.append((apk, build_options))
-         except ProgramFailedError, e:
-             print e
-             print e.stdout
-     return apks
+            # Optionally sign the API file
+            if build_options.get('sign', True):
+                password = raw_input('Enter keystore password:')
+                if not password:
+                    print "Not signing the package."
+                else:
+                    apk.sign(env.keystore, env.keyalias, password)
+
+            # Align
+            apk.align()
+
+            apks.append((apk, build_options))
+        except ProgramFailedError, e:
+            print e
+            print e.stdout
+    return apks
 
 
 def deploy():
@@ -97,10 +105,10 @@ def deploy():
         print "Then do that first, and commit them...."
         return
 
+    # Build all APKs, upload them all the the server
     for apk, build_options in build(clean=True):
         if build_options.get('deploy', True):
             put(apk.filename, env.deploy_dir)
 
     put('CHANGES', env.deploy_dir)
     run('sudo /etc/init.d/lighttpd restart')
-

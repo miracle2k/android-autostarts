801ec5839fa7975bfb8286a99938c3441e927ae8
Michael Elsdörfer <michael@elsdoerfer.com>
2012-08-13 22:48:12 +0200

    For Vodaphone store, disable root toggle feature.

---
+++ src-opt/amazon/src/com/elsdoerfer/android/autostarts/opt/RootFeatures.java
@@ -0,0 +1,12 @@
+package src.com.elsdoerfer.android.autostarts.opt;
+
+import android.content.Context;
+
+/**
+ * Amazon-specific functionality.
+ */
+public class RootFeatures {
+
+	public static final boolean Enabled = true;
+
+}
---
+++ src-opt/vodaphone/src/com/elsdoerfer/android/autostarts/opt/RootFeatures.java
@@ -0,0 +1,11 @@
+package src.com.elsdoerfer.android.autostarts.opt;
+
+
+/**
+ * Vodaphone App-Select-specific functionality.
+ */
+public class RootFeatures {
+
+	public static final boolean Enabled = false;
+
+}

527535c6b6e24a8d5c33c0ec70ad7c297152a555
Michael Elsdörfer <michael@elsdoerfer.com>
2017-08-05 19:06:52 +0100

    New build scripts.

---
+++ cli/build.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+tx pull -a
+# Workaround for Android not supporting this language (no ISO-639-2 support?)
+rm locale/*-ast.po
+
+
+buck build //:app
+buck build //:zipalign
+
+VERSION=$(aapt d badging ./buck-out/gen/zipalign/zipalign.apk | grep 'versionName=' | awk -F: 'match($0,"versionName="){ print substr($2,RSTART+4)}' | tr -d "'")
+cp ./buck-out/gen/zipalign/zipalign.apk Android-Autostarts-Full-$VERSION.apk
+cp ./buck-out/gen/zipalign/zipalign.apk ./buck-out/gen/zipalign/Android-Autostarts-Full.apk
+
+
+( cd ./buck-out/gen/zipalign/ && bundlecop submit \
+  --projectKey $BUNDLECOP_AUOSTARTS_PROJECT_KEY \
+  --bundleset 268318521568755716 \
+  Android-Autostarts-Full.apk )
---
+++ cli/deploy.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+#
+aws s3 cp CHANGES s3://elsdoerfer-site-downloads/android-autostarts/
+aws s3 ls Android-Autostarts-*.apk s3://elsdoerfer-site-downloads/android-autostarts/
